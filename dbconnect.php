89b202c5aa8ec00d62c0d44ecf1195ce70d78a14
Rebecca Wright <rwright@mail.umw.edu>
2012-02-24 19:28:33 -0500

    Changes to the database. We now have a dbconnect. This should work for the forms with the primary key.

---
+++ dbconnect.php
@@ -0,0 +1,4 @@
+<?php
+$db = mysqli_connect('localhost', 'assist', 'scoutpw', 'cubScout')
+ or die ("ERROR: connecting to mysql server!");
+?>
---
+++ popcorndb.sql
@@ -0,0 +1,13 @@
+﻿DROP DATABASE IF EXISTS cubScout;
+CREATE DATABASE IF NoT EXISTS cubScout;
+GRANT ALL PRIVILEGES ON cubScout.* to 'assist'@'localhost' identified by 'scoutpw';
+USE cubScout;
+CREATE TABLE popcorn_sales(
+	id int NOT NULL auto_increment,
+	firstname VARCHAR(25),
+	lastname VARCHAR(30),
+	product varchar(50) DEFAULT NULL,
+	quantity int(11) DEFAULT NULL,
+	price decimal(5,2) DEFAULT NULL,
+	PRIMARY KEY (id)
+);
